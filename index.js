a8acf7a9435d20573a9a968f2f3b82014a670b8b
susmoysaha6 <susmoysaha6@gmail.com>
2022-11-17 04:14:16 +0600

    /jwt access token created

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const e = require('express');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const port = process.env.PORT || 5000;
@@ -115,6 +115,17 @@ async function run() {
             res.send(result);
         })
 
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' });
+                return res.send({ accessToken: 'token' });
+            }
+            console.log(user);
+            res.status(403).send({ accessToken: '' })
+        })
 
         app.post('/users', async (req, res) => {
             const user = req.body;
